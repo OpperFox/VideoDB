ee4386fcdcf7587fd715d4d0cb5907344878bae6
Filoptemon <eru.eternum23@gmail.com>
2025-07-27 21:23:56 -0500

    no deja crear usuarios repetidos

--- src/GUI/RegistrarseGUI.java
+++ src/GUI/RegistrarseGUI.java
@@ -86,7 +86,7 @@ public class RegistrarseGUI extends JFrame {
                 String clave = new String(campoClave.getPassword()); // Se convierte el arreglo de caracteres en String
 
                 // Se valida que ambos campos no estén vacíos
-                if (!usuario.isEmpty() && !clave.isEmpty()) {
+                if (!usuario.isEmpty() && !clave.isEmpty() && !SQL.Query.user_exists(SQL.DBConnection.getConnection(), usuario, clave)) {
                     
                 	SQL.Query.user_registry(SQL.DBConnection.getConnection(), usuario, clave);
                 	
